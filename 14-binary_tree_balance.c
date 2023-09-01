c2fc07265f4529ff0f21a4d4feced35c1a9020e3
HarpLoid <walesii19@gmail.com>
2023-09-01 11:06:24 +0100

    fixed typo error from debugging

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,7 +12,6 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	size_t l = 0, r = 0, balance_factor = 0;
-	int c = 0;
 
 	if (tree)
 	{
@@ -36,7 +35,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
-	int c = 0;
 
 	if (tree)
 	{
