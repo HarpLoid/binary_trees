a97bb629f53da3062b50624e9ccf58d74bae6e92
HarpLoid <walesii19@gmail.com>
2023-09-01 11:02:20 +0100

    rewrote bin_tree_height and fixed errors in balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,14 +12,47 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	size_t l = 0, r = 0, balance_factor = 0;
+	int c = 0;
 
 	if (tree)
 	{
-		l = (tree->left) ? binary_tree_balance(tree->left) + 1 : 0;
-		r = (tree->right) ? binary_tree_balance(tree->right) + 1 : 0;
+		l = (tree->left) ? binary_tree_height(tree->left) + 1 : 0;
+		printf("c: %d | l: %ld\n", c += 1, l);
+		r = (tree->right) ? binary_tree_height(tree->right) + 1 : 0;
+		printf("c: %d | r: %ld\n", c += 1, r);
 
-		balance_factor = l - r;
+		balance_factor = (l - r);
+		printf("c: %d | b_f: %ld\n", c += 1, balance_factor);
 	}
 
 	return (balance_factor);
 }
+
+/**
+ * binary_tree_height - measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: height of tree or
+ * 0 if  tree is NULL
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+	int c = 0;
+
+	if (tree)
+	{
+		left_height = (tree->left) ? binary_tree_height(tree->left) + 1 : 0;
+		printf("c: %d | l: %ld\n", c += 1, left_height);
+		right_height = (tree->right) ? binary_tree_height(tree->right) + 1 : 0;
+		printf("c: %d | r: %ld\n", c += 1, right_height);
+
+		if (left_height > right_height)
+			return (left_height);
+		else
+			return (right_height);
+	}
+
+	return (0);
+}
+
