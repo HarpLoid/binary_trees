d753a5b058dd090e3524a218e60055c61e28f730
HarpLoid <walesii19@gmail.com>
2023-09-01 13:25:14 +0100

    fixed logic error

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,15 +12,17 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int is_full;
+	int is_full = 1;
 
-	if (tree)
-	{
-		is_full = ((tree->left && !tree->right) || (!tree->left && tree->right)
-				   || !binary_tree_is_full(tree->left)
-				   || !binary_tree_is_full(tree->right))
-				   ? 0 : 1;
-	}
+	if (!tree)
+		return (is_full);
 
-	return (is_full);
+	if (!tree->left && !tree->right)
+		return (is_full);
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) &&
+				binary_tree_is_full(tree->right));
+
+	return (0);
 }
